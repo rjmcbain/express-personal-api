d507ce95e93000e03f2fbf9e22bc2a59951e305b
rjmcbain <rmcbain01@gmail.com>
2018-01-03 17:30:31 -0700

    updated

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.get('/api', function api_index(req, res) {
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/rjmcbain/express-personal-api/blob/master/README.md", // CHANGE ME
+    base_url: "http://videogames.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Dis About Me"}, // CHANGE ME
+      {method: "POST", path: "/api/videogames", description: "E.g. Create a new video game"} // CHANGE ME
     ]
   })
 });
@@ -59,4 +59,3 @@ app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
 });
 
-// app.listen(process.env.PORT || 3000)
